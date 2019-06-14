a655d4aa07f365a76decb88679aa7ca462228127
bcdnko <bcdnko@gmail.com>
2019-06-14 09:33:19 +0700

    Basic interfaces

---
+++ src/app/bible/bible.interfaces.ts
@@ -0,0 +1,46 @@
+import {
+  Language,
+  LanguageId,
+} from '../core/language.interfaces';
+
+export type BibleVersionId = string;
+export type BibleBookId = number;
+export type BibleBookAlias = string;
+export type TestamentId = 'old' | 'new';
+export type CrossVersionId = number;
+
+export interface BibleLanguage extends Language {
+  defaultVersionId: BibleVersionId;
+}
+
+export interface BibleVersion {
+  id: BibleVersionId;
+  title: string;
+  titleShort?: string;
+  langId: LanguageId;
+  copyright?: string;
+  version?: string;
+  updated: string;
+}
+
+export interface BibleBook {
+  id: BibleBookId;
+  cvId: CrossVersionId;
+  versionId: BibleVersionId;
+  title: string;
+  titleShort?: string;
+  chapters: number;
+  testament: TestamentId;
+  description?: string;
+  aliases?: BibleBookAlias[];
+}
+
+export interface BibleVerse {
+  id: number;
+  cvId: CrossVersionId;
+  bookId: BibleBookId;
+  chapter: number;
+  no: number;
+  text: string;
+}
+

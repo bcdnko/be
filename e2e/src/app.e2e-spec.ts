1699db75caea8a59f2ef358c0037878c9ae4c451
bcdnko <bcdnko@gmail.com>
2020-05-03 16:48:09 +0700

    Renamed to be

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to emmaus!');
+    // expect(page.getTitleText()).toEqual('Welcome to BibleExplorer!');
   });
 
   afterEach(async () => {

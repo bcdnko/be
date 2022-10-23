a59a89d0a7d473f6e08ed0acf6d7c70cafb76a6b
bcdnko <bcdnko@gmail.com>
2022-10-23 19:05:28 +0700

    Updated strong importer

--- src/cli/import-strong.ts
+++ src/cli/import-strong.ts
@@ -16,19 +16,25 @@ function saveDict(id, dict: StrongDictionary): void {
   fs.writeFileSync(filePath, JSON.stringify(dict));
 }
 
+function saveWordDict(strongId, wordId, dict: StrongDictionary): void {
+  const filePath = path.join(paths.strongDicts, strongId, wordId + '.json');
+  mkdirp.sync(path.join(paths.strongDicts, strongId));
+  fs.writeFileSync(filePath, JSON.stringify(dict));
+}
+
 (async () => {
   try {
     const args = parseArguments();
     if (!args['id'] || typeof args['id'] !== 'string' || !args['id'].length) {
-      throw new Error('Stong ID was not provided. Please specify it by using -id ID');
+      throw new Error('Strong ID was not provided. Please specify it by using -id ID');
     }
 
-    const importId = args['id'];
-    const parserDir = path.join(paths.cliScripts, 'import-strong', importId);
-    const parserPath = path.join(parserDir, importId + '-parser');
+    const strongId = args['id'];
+    const parserDir = path.join(paths.cliScripts, 'import-strong', strongId);
+    const parserPath = path.join(parserDir, strongId + '-parser');
 
     const Parser = await import(parserPath);
-    const dataPath = path.join(paths.strongImportsSource, importId);
+    const dataPath = path.join(paths.strongImportsSource, strongId);
 
     console.log('Importing from: ' + dataPath);
     const parser = new Parser.default(dataPath, parserDir);
@@ -38,13 +44,14 @@ function saveDict(id, dict: StrongDictionary): void {
 
     while (word = await parser.parseStrong()) {
       dict[word.id] = word;
+      saveWordDict(strongId, word.id, word)
     }
 
-    saveDict(importId, dict);
+    saveDict(strongId, dict);
 
     console.log('done\n');
   } catch (e) {
-    throw e;
     console.log(`Error: ${e.message}`);
+    console.log('Usage example: npm run import-strong -- -id mb-strong-ru');
   }
 })();
